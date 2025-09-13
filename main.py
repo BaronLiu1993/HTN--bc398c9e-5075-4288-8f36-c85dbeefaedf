8a6a71efd2e21a7cfe705493707eff50c7d15ecc
Jie Xuan Liu <117589118+BaronLiu1993@users.noreply.github.com>
2025-09-13 15:16:06 -0400

    initial commit

---
+++ main.py
@@ -0,0 +1,33 @@
+
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+from service.rewriteLayer import rewrite
+
+from service.scraperLayer import scraper
+
+app = FastAPI()
+def executeAgents(initialInput: str):
+    variable = scraper(initialInput)
+    variable2 = rewrite(variable)
+    return {'variable': variable, 'variable2': variable2}
+
+class InputSchema(BaseModel):
+    initialInput: str
+
+@app.post("/execute-agent-workflow")
+def executeAgentWorkflow(request: InputSchema):
+    print(request)
+    try:
+        response = executeAgents(request.initialInput)
+        return response
+    except Exception as e:
+        print(e)
+        raise Exception(e)
+
+@app.get("/health")
+def health():
+    try:
+        return { "message": "Hi HackTheNorth! The container is healthy!"}
+    except Exception as e:
+        raise Exception(e)
